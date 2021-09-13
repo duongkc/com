7fdce67421aaa15e1ed820bcdcbf1d38b7b53168
KC <kimchauduong@gmail.com>
2021-09-13 16:13:22 +0200

    initial commit

---
+++ example/ikm/MealPreparation.java
@@ -0,0 +1,13 @@
+package com.example.ikm;
+
+/**
+ * Class to
+ *
+ * @author Imke, Kim, Melany
+ * @version 1.0
+ */
+public class MealPreparation {
+    public static void main(String[] args) {
+
+    }
+}
